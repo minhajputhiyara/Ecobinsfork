2250e575d5da8801721d3d3ee8971e22eadf3a59
aioont <aioont8@gmail.com>
2024-06-26 21:09:15 +0530

    React 3 page by aioont

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import './styles/App.css';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import RecyclingGuide from './components/RecyclingGuide/RecyclingGuide';
+import DisposalGuide from './components/DisposalGuide/DisposalGuide';
+import SearchPage from './components/SearchPage/SearchPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Recycling Guide</Link>
+            </li>
+            <li>
+              <Link to="/disposal">Disposal Guide</Link>
+            </li>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<RecyclingGuide />} />
+          <Route path="/disposal" element={<DisposalGuide />} />
+          <Route path="/search" element={<SearchPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
